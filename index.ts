42c784a23fc7183ae4ace2a2379e4ed07b198ebb
Anwesh43 <anweshthecool0@gmail.com>
2021-04-25 18:25:58 +0530

    adding LRRBNode class

--- index.ts
+++ index.ts
@@ -144,3 +144,45 @@ class Animator {
         }
     }
 }
+
+class LRRBNode {
+
+    prev : LRRBNode 
+    next : LRRBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LRRBNode(this.i + 1)
+            this.next.prev = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLRRBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+       this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LRRBNode {
+        var curr : LRRBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
